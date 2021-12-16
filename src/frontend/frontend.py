6e0f2b9a380f0a11f8f00991ca68eeb3292931f6
yaelkot <64592523+yaelkot@users.noreply.github.com>
2021-12-16 14:31:22 +0200

    did wrong input popup and add images

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,9 +1,20 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
+from kivy.network.urlrequest import UrlRequest
+from kivy.graphics import Rectangle, Color
 
 kv = Builder.load_file("my.kv")
+
+
+class Popups(FloatLayout):
+    pass
+
 
 class grid(Widget):
 
@@ -11,19 +22,39 @@ class grid(Widget):
     time_duration = ObjectProperty(None)
     num_recommendations = ObjectProperty(None)
 
+    def show_popup(self, title):
+        show = Popups()
+        popupWindow = Popup(title=title, content=show, size_hint=
+        (0.5,0.6))
+        popupWindow.open()
+        # Attach close button press with popup.dismiss action
+        # content.bind(on_press = popup.dismiss)
 
     def btn(self):
-        print("start location:", self.start_location.text, "time duration: ", self.time_duration.text, "number of recommendations: ", self.num_recommendations.text)
-        self.start_location = ""
-        self.time_duration = ""
-        self.num_recommendations = ""
+        if any(char.isdigit() for char in self.start_location.text) or not self.time_duration.text.isdigit() or not self.num_recommendations.text.isdigit():
+            self.show_popup(title="Wrong Input")
+        #http reguest with the given parameters
+        else:
+            lst = self.start_location.text.split(" ")
+            start_stat = lst[0]
+            for word in lst[1:]:
+                start_stat = start_stat + "+" + word
+            str_url = "http://127.0.0.1:5000/?startlocation=" + start_stat
+            str_url += "&timeduration=" + self.time_duration.text + "&k=" + self.num_recommendations.text
+            req = UrlRequest(url=str_url)
+            if req.error != None:
+                pass
+            elif req.result != None:
+                pass
+        self.start_location.text = ""
+        self.time_duration.text = ""
+        self.num_recommendations.text = ""
 
 
 class app(App):
 
     def build(self):
         return grid()
-        # return kv
 
 if __name__ == "__main__":
     app().run()
